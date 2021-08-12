d69e6cea9e4fd66ee9d8da158e4a603b8a435411
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-12 16:22:28 +0530

    [Devesh]: Added condition for bet won or not

--- src/com/gamblersimulation/Gamblersimulation.java
+++ src/com/gamblersimulation/Gamblersimulation.java
@@ -1,11 +1,26 @@
 package com.gamblersimulation;
 
 public class Gamblersimulation {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 		//Start with 100 every day and for every game 1 condition initialised.
 		int everyday = 100 ;
-		final int everygame = 1 ;
+		int everygame = 1 ;
 		
+		int totalstake = everyday;
+		
+		if ( winprob() == 1 ) //for win
+             everyday += everygame ;
+        else                // for loss
+             everyday -= everygame;
+		
+		if (everyday>100) System.out.println("Player won bet");
+		else System.out.println("Player lost bet");
+	
 	}
+	
+	public static int winprob(){
+        int prob = (int)((Math.random()*10)%2);
+        return prob;
+    }
 }
