8ee0520e5d48a8754d0b4f3c5681429530a2e89c
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-12 16:19:12 +0530

    [Devesh]: Added condition for everyday and everygame stake

--- src/com/gamblersimulation/Gamblersimulation.java
+++ src/com/gamblersimulation/Gamblersimulation.java
@@ -1,7 +1,11 @@
 package com.gamblersimulation;
 
 public class Gamblersimulation {
-	public class void main(String[] args) {
+	public static void main(String[] args) {
+		
+		//Start with 100 every day and for every game 1 condition initialised.
+		int everyday = 100 ;
+		final int everygame = 1 ;
 		
 	}
 }
